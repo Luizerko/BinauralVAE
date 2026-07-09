Going back to original beta computation for stft 4 channel as well, and fixing inference pipeline for proper audio reconstruction.

inference.py
@@ -1,7 +1,9 @@
 import argparse
 import os
+import sys
 
 import torch
+import torchaudio
 import matplotlib.pyplot as plt
 import numpy as np
 import soundfile as sf
@@ -16,13 +18,13 @@ def infer_and_plot(args):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     dataset = BinauralDataset(args.dataset_dir, args.dataset_method)
     
-    if args.dataset_index >= len(dataset) or args.dataset_index < 0:
+    if args.dataset_idx_ini >= len(dataset) or args.dataset_idx_ini < 0 or args.dataset_idx_end >= len(dataset) or args.dataset_idx_end < 0:
         print(f'Dataset index out of bound. Dataset size: {len(dataset)}')
         return
 
     # Fetching the original sample
-    original_sample = dataset[args.dataset_index]
-    sample_shape = original_sample.shape
+    original_samples = torch.stack([dataset[i] for i in range(args.dataset_idx_ini, args.dataset_idx_end)])
+    sample_shape = original_samples[0].shape
     
     # Initializing the model
     if args.dataset_method in ['mel', 'stft_4ch']:
@@ -35,21 +37,23 @@ def infer_and_plot(args):
     model.eval()
 
     # Running inference
-    x = original_sample.unsqueeze(0).to(device)
+    x = original_samples.to(device)
     with torch.no_grad():
-        reconstruction, mu, logvar = model(x)
+        reconstructions, _, _ = model(x)
         
-    # Removing batch dimension and moving back to CPU for plotting
-    original_data = original_sample.cpu().numpy()
-    reconstructed_data = reconstruction.squeeze(0).cpu().numpy()
+    # Moving back to CPU for plotting
+    original_data = original_samples.cpu().numpy()
+    reconstructed_data = reconstructions.cpu().numpy()
 
     # Plotting reconstruction and then actually reconstructing audio
     if args.dataset_method == 'mel':
-        plot_mel(original_data, reconstructed_data)
-        reconstruction_mel(reconstructed_data, args.output_file)
+        plot_mel(original_data[0], reconstructed_data[0])
+        reconstruction_mel(original_data, 'original.wav', args.n_samples, args.mel_bands, args.hop_len, args.sample_rate, args.rec_method)
+        reconstruction_mel(reconstructed_data, args.output_file, args.n_samples, args.mel_bands, args.hop_len, args.sample_rate, args.rec_method)
 
     elif args.dataset_method == 'stft_4ch':
-        plot_stft_4ch(original_data, reconstructed_data)
+        plot_stft_4ch(original_data[0], reconstructed_data[0])
+        reconstruction_stft_4ch(original_data, 'original.wav', args.n_samples, args.hop_len, args.sample_rate)
         reconstruction_stft_4ch(reconstructed_data, args.output_file, args.n_samples, args.hop_len, args.sample_rate)
 
     return original_data, reconstructed_data
@@ -114,34 +118,75 @@ def plot_stft_4ch(original, reconstruction):
 
 
 # Reconstructing audio from Mel model's output
-def reconstruction_mel(data, output_file='output_mel.wav'):
+def reconstruction_mel(data, output_file='output_mel.wav', n_fft=2048, n_mels=128, hop_len=147, sample_rate=44100, rec_method='GriffinLim'):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    
-    # Loading BigVGAN-v2 model to map mel spectrogram back to audio. The used checkpoint used hop length of 512 instead of our 147, so the audio is probably going to sound slow
-    import bigvgan
-    model = bigvgan.BigVGAN.from_pretrained('nvidia/bigvgan_v2_44khz_128band_512x', use_cuda_kernel=False).to(device)
-    model.remove_weight_norm()
-    model.eval()
-
-    # Processing spectrograms separately since vocoders are normally designed for mono audio, not stereo, including BigVGAN-v2
     mel_tensor = torch.FloatTensor(data).to(device)
-    with torch.no_grad():
-        wav_l = model(mel_tensor[0].unsqueeze(0)).squeeze().cpu().numpy()
-        wav_r = model(mel_tensor[1].unsqueeze(0)).squeeze().cpu().numpy()
-    stereo_wav = np.stack([wav_l, wav_r], axis=1)
 
-    sf.write(output_file, stereo_wav, 44100)
-    print(f"Saved Mel reconstruction to {output_file}")
+    # Unormalizing data
+    stats = torch.load('data/mel_stats.pt', map_location=device)
+    mel_min, mel_max = stats['min'], stats['max']
+    mel_tensor = (mel_tensor * (mel_max - mel_min + 1e-8)) + mel_min
+
+    # Converting dB to power and then to magnitude
+    ref_power = 10000.0 
+    mel_tensor = ref_power * (10.0 ** (mel_tensor / 10.0))
+    mel_tensor = torch.sqrt(mel_tensor)
+
+    if rec_method == 'BigVGAN':
+        # Loading BigVGAN-v2 model to map mel spectrogram back to audio. The used checkpoint used hop length of 512 instead of our 147, so the audio is probably going to sound slow
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        sys.path.append(os.path.join(current_dir, 'bigvgan'))
+        
+        import bigvgan
+        model = bigvgan.BigVGAN.from_pretrained('nvidia/bigvgan_v2_44khz_128band_256x', use_cuda_kernel=False).to(device)
+        model.remove_weight_norm()
+        model.eval()
+
+        # Processing spectrograms separately since vocoders are normally designed for mono audio, not stereo, including BigVGAN-v2
+        mel_tensor = torch.log(torch.clamp(mel_tensor, min=1e-5))
+        with torch.no_grad():
+            wav_l = model(mel_tensor[:, 0]).cpu().numpy()
+            wav_r = model(mel_tensor[:, 1]).cpu().numpy()
+    
+    elif rec_method == 'GriffinLim':
+        # Converiting Mel spectrogram to (magnitude) SFTF and then running Griffim-Lim method
+        inverse_mel = torchaudio.transforms.InverseMelScale(n_stft=n_fft//2 + 1, n_mels=n_mels, sample_rate=sample_rate).to(device)
+        griffin_lim = torchaudio.transforms.GriffinLim(n_fft=n_fft, hop_length=hop_len, power=1.0).to(device)
+    
+        with torch.no_grad():
+            linear_stft_l = inverse_mel(mel_tensor[:, 0])
+            wav_l = griffin_lim(linear_stft_l).cpu().numpy()
+            
+            linear_stft_r = inverse_mel(mel_tensor[:, 1])
+            wav_r = griffin_lim(linear_stft_r).cpu().numpy()
+
+    # Peak-normalization and audio saving
+    wav_l = wav_l.flatten()
+    wav_r = wav_r.flatten()
+    stereo_wav = np.stack([wav_l, wav_r], axis=1)
+    max_amplitude = np.max(np.abs(stereo_wav))
+    if max_amplitude > 0:
+        stereo_wav = stereo_wav / (max_amplitude + 1e-9)
+    sf.write(output_file, stereo_wav, sample_rate)
 
 
 # Reconstructing audio from STFT-4ch model's output
 def reconstruction_stft_4ch(data, output_file='output_stft.wav', n_fft=1024, hop_length=147, sample_rate=44100):
     # Converting to tensors
-    mag_l, phase_l, mag_r, phase_r = data
-    mag_l = torch.from_numpy(mag_l)
-    phase_l = torch.from_numpy(phase_l)
-    mag_r = torch.from_numpy(mag_r)
-    phase_r = torch.from_numpy(phase_r)
+    mag_l = torch.from_numpy(data[:, 0])
+    phase_l = torch.from_numpy(data[:, 1])
+    mag_r = torch.from_numpy(data[:, 2])
+    phase_r = torch.from_numpy(data[:, 3])
+
+    # Unormalizing data
+    stats = torch.load('data/stft_stats.pt')
+    mag_min, mag_max = stats['mag_min'], stats['mag_max']
+    
+    mag_l = (mag_l * (mag_max - mag_min + 1e-8)) + mag_min
+    mag_r = (mag_r * (mag_max - mag_min + 1e-8)) + mag_min
+
+    phase_l = (phase_l * (2 * np.pi)) - np.pi
+    phase_r = (phase_r * (2 * np.pi)) - np.pi
 
     # Mapping magnitude and angle back to complex numbers using Euler's formula (z = m * e^(i * phi))
     complex_l = mag_l * torch.exp(1j * phase_l)
@@ -153,9 +198,10 @@ def reconstruction_stft_4ch(data, output_file='output_stft.wav', n_fft=1024, hop
     wav_r = torch.istft(complex_r, n_fft=n_fft, hop_length=hop_length, window=window, return_complex=False)
 
     # Stacking stereo in [Time, Channels] and saving
-    stereo_wav = torch.stack([wav_l, wav_r], dim=1).numpy()
+    wav_l = wav_l.flatten().numpy()
+    wav_r = wav_r.flatten().numpy()
+    stereo_wav = np.stack([wav_l, wav_r], axis=1)
     sf.write(output_file, stereo_wav, sample_rate) 
-    print(f"Saved STFT reconstruction to {output_file}")
 
 
 if __name__ == '__main__':
@@ -163,11 +209,14 @@ if __name__ == '__main__':
 
     # Parsing arguments
     parser.add_argument("--checkpoint_path", type=str, default='models/checkpoints/', help="Path to the saved model checkpoint")
-    parser.add_argument("--dataset_index", type=int, required=True, help="Index of the sample in the dataset to reconstruct")
-    parser.add_argument("--dataset_dir", type=str, default='data/dataset/', help="Path to processed .pt dataset directory")
-    parser.add_argument("--dataset_method", type=str, default='mel', help="Method of audio processing used", choices=['mel', 'stft_4ch', 'stft_complex', 'wave2vec'])
     parser.add_argument("--run_name", type=str, default='test_run_0001', help="Run name for proper Tensorboard visualization")
-    parser.add_argument("--output_file", type=str, default='output.wav', help="Path to the output file")
+    parser.add_argument("--output_file", type=str, default='reconstructed.wav', help="Path to the output file")
+    parser.add_argument("--dataset_dir", type=str, default='data/dataset/', help="Path to processed .pt dataset directory")
+
+    parser.add_argument("--dataset_idx_ini", type=int, required=True, help="Index of initial sample in the dataset to reconstruct")
+    parser.add_argument("--dataset_idx_end", type=int, required=True, help="Index of final sample in the dataset to reconstruct")
+    parser.add_argument("--dataset_method", type=str, default='mel', help="Method of audio processing used", choices=['mel', 'stft_4ch', 'stft_complex', 'wave2vec'])
+    parser.add_argument("--rec_method", type=str, default='GriffinLim', help="Reconstruction method to be used (only for Mel spectrograms)", choices=['GriffinLim', 'BigVGAN'])
 
     parser.add_argument("--latent_dim_pow", type=int, default=5, help="Size of the latent space (in powers of 2)")
     parser.add_argument("--n_filters", type=int, default=3, help="Number of conv layers")
@@ -177,9 +226,10 @@ if __name__ == '__main__':
     parser.add_argument("--stride_h", type=int, default=1, help="Horizontal stride")
     parser.add_argument("--pad", type=int, default=0, help="Amount of padding")
 
+    parser.add_argument("--n_samples", type=int, default=2048, help="Amount of samples used for Mel/STFT processing. Important metric for reconstruction")
+    parser.add_argument("--mel_bands", type=int, default=128, help="Amount of Mel bands used. Important metric for Mel reconstruction")
+    parser.add_argument("--hop_len", type=int, default=147, help="Time resolution used for Mel/STFT processing. Important metric for reconstruction")
     parser.add_argument("--sample_rate", type=int, default=44100, help="Sample rate of original audio. Important for reconstruction")
-    parser.add_argument("--n_samples", type=int, default=0, help="Amount of samples used for STFT processing. Important metric for reconstruction")
-    parser.add_argument("--hop_len", type=int, default=0, help="Time resolution used for STFT processing. Important metric for reconstruction")
 
     args = parser.parse_args()
 