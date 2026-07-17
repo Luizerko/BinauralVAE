Fixing (some) CVAE architectural bugs, normalizing data processing for complex numbers as well, implementing inference for CVAE, fixing training bugs for CVAE and implementing dream pipeline for all models.

data/data_processing.py
@@ -155,6 +155,27 @@ def dataset_create_stft_4ch(src_dir, tgt_dir, num_seeds):
 
 # Creating and saving STFT complex dataset
 def dataset_create_stft_complex(src_dir, tgt_dir, num_seeds):
+    # Computing normalization statistics
+    global_mag_max = float('-inf')
+    for seed in range(1, num_seeds+1):
+        # Excepting broken simulation runs
+        try:
+            src_file = os.path.join(src_dir, f'seed_{seed}', 'split_stft.npz')
+            data = np.load(src_file)
+            stft_left = data['left']
+            stft_right = data['right']
+            
+            seed_mag_max = max(np.abs(stft_left).max(), np.abs(stft_right).max())
+            
+            if seed_mag_max > global_mag_max:
+                global_mag_max = seed_mag_max
+        except:
+            continue
+
+    # Saving normalization statistics for later audio reconstruction
+    stats = {'mag_max': float(global_mag_max)}
+    torch.save(stats, 'complex_stats.pt')
+    
     for seed in range(1, num_seeds+1):
         # Excepting broken simulation runs
         try:
@@ -171,8 +192,8 @@ def dataset_create_stft_complex(src_dir, tgt_dir, num_seeds):
             # Iterating through the data to save complex binaural "image"
             num_chunks = stft_left.shape[0]
             for i in range(num_chunks):
-                chunk_left = stft_left[i]
-                chunk_right = stft_right[i]
+                chunk_left = stft_left[i]/global_mag_max
+                chunk_right = stft_right[i]/global_mag_max
                 chunk_binaural = np.stack((chunk_left, chunk_right), axis=0)
                 chunk_binaural = torch.tensor(chunk_binaural, dtype=torch.complex64)
                 torch.save(chunk_binaural, os.path.join(seed_tgt_dir, f'binaural_image_{i+1}.pt'))

inference.py
@@ -4,6 +4,8 @@ import sys
 
 import torch
 import torchaudio
+from torch.utils.data import DataLoader
+
 import librosa
 import matplotlib.pyplot as plt
 import numpy as np
@@ -37,12 +39,17 @@ def infer_and_plot(args):
 
     # Running inference
     x = original_samples.to(device)
+    loader = DataLoader(x, batch_size=args.batch_size, shuffle=False)
     with torch.no_grad():
-        if args.dataset_method in ['mel', 'stft_4ch']:
-            reconstructions, _, _ = model(x)
-        elif args.dataset_method == 'stft_complex':
-            reconstructions, _, _, _ = model(x)
-        
+        reconstructions = []
+        for batch in loader:
+            if args.dataset_method in ['mel', 'stft_4ch']:
+                reconstruction, _, _ = model(batch)
+            elif args.dataset_method == 'stft_complex':
+                reconstruction, _, _, _ = model(batch)
+            reconstructions.append(reconstruction)
+    reconstructions = torch.cat(reconstructions, dim=0)
+
     # Moving back to CPU for plotting
     original_data = original_samples.cpu().numpy()
     reconstructed_data = reconstructions.cpu().numpy()
@@ -261,6 +268,12 @@ def reconstruction_stft_complex(data, output_file='output_complex.wav', n_fft=10
     complex_l = np.concatenate(data[:, 0], axis=1)
     complex_r = np.concatenate(data[:, 1], axis=1)
 
+    # Unormalizing data
+    stats = torch.load('data/complex_stats.pt')
+    mag_max = stats['mag_max']
+    complex_l = complex_l*mag_max
+    complex_r = complex_r*mag_max
+
     # Inverse STFT with the same window, sample and hop length as the forward process 
     wav_l = librosa.istft(complex_l, hop_length=hop_length, n_fft=n_fft)
     wav_r = librosa.istft(complex_r, hop_length=hop_length, n_fft=n_fft)
@@ -285,6 +298,7 @@ if __name__ == '__main__':
     parser.add_argument("--seed_idx", type=int, required=True, help="Index of the seed to reconstruct")
     parser.add_argument("--dataset_method", type=str, default='mel', help="Method of audio processing used", choices=['mel', 'stft_4ch', 'stft_complex'])
     parser.add_argument("--rec_method", type=str, default='gl', help="Reconstruction method to be used (only for Mel spectrograms)", choices=['gl', 'bvg'])
+    parser.add_argument("--batch_size", type=int, default=256, help="Batch size")
 
     parser.add_argument("--latent_dim_pow", type=int, default=5, help="Size of the latent space (in powers of 2)")
     parser.add_argument("--n_filters", type=int, default=3, help="Number of conv layers")

models/CVAE.py
@@ -69,7 +69,7 @@ class CVAE(nn.Module):
 
         # Defining decoder
         self.decoder_input_1 = ComplexLinear(self.latent_dim, self.latent_dim//2)
-        self.decoder_input_1 = ComplexLinear(self.latent_dim//2, in_features)
+        self.decoder_input_2 = ComplexLinear(self.latent_dim//2, in_features)
         decoder = [nn.Unflatten(1, (2**(9-(n_filters-1)), v_in_features, h_in_features))]
         for i in range(n_filters):
             if i == n_filters-1:
@@ -83,22 +83,28 @@ class CVAE(nn.Module):
         self.decoder = nn.Sequential(*decoder)
 
     # Initialization function
-    def init_weights(self, m, method='torch_default'):
-        if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.Linear)):
-            if method == 'he':
-                nn.init.kaiming_uniform_(m.weight, nonlinearity='relu')
-            elif method == 'xavier':
-                nn.init.xavier_uniform_(m.weight)
-            elif method == 'torch_default':
-                nn.init.kaiming_uniform_(m.weight, a=math.sqrt(5))
+    # def init_weights(self, m, method='torch_default'):
+    #     if isinstance(m, (ComplexConv2d, ComplexConvTranspose2d, ComplexLinear)):
+    #         if method == 'he':
+    #             nn.init.kaiming_uniform_(m.weight.real, nonlinearity='relu')
+    #             nn.init.kaiming_uniform_(m.weight.imag, nonlinearity='relu')
+    #         elif method == 'xavier':
+    #             nn.init.xavier_uniform_(m.weight.real)
+    #             nn.init.xavier_uniform_(m.weight.imag)
+    #         elif method == 'torch_default':
+    #             nn.init.kaiming_uniform_(m.weight.real, a=math.sqrt(5))
+    #             nn.init.kaiming_uniform_(m.weight.imag, a=math.sqrt(5))
             
-            if m.bias is not None:
-                if method == 'torch_default':
-                    fan_in, _ = nn.init._calculate_fan_in_and_fan_out(m.weight)
-                    bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
-                    nn.init.uniform_(m.bias, -bound, bound)
-                else:
-                    nn.init.constant_(m.bias, 0)
+    #         if m.bias is not None:
+    #             if method == 'torch_default':
+    #                 fan_in, _ = nn.init._calculate_fan_in_and_fan_out(m.weight)
+    #                 bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
+    #                 nn.init.uniform_(m.bias.real, -bound, bound)
+    #                 nn.init.uniform_(m.bias.imag, -bound, bound)
+    #             else:
+    #                 nn.init.constant_(m.bias.real, 0)
+    #                 nn.init.constant_(m.bias.imag, 0)
+
 
     # Complex Reparametrization trick
     def reparameterize(self, mu, sigma, delta):
@@ -130,7 +136,8 @@ class CVAE(nn.Module):
 
         z = self.reparameterize(mu, sigma, delta)
         
-        h_dec = self.decoder_input(z)
+        h_dec = self.decoder_input_1(z)
+        h_dec = self.decoder_input_2(h_dec)
         return self.decoder(h_dec), mu, sigma, delta
     
     # Loss function rec_loss + beta*kl_loss